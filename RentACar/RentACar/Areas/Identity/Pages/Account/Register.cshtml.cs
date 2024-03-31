13ed58d4846b4170454cad7fa7a46c43d2647739
machev24 <machevivan3@gmail.com>
2024-03-31 17:20:54 +0300

    register and login forms config

--- RentACar/RentACar/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RentACar/RentACar/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,19 +45,35 @@ namespace RentACar.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
             public string Email { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            // New properties
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Unique Citizen Number")]
+            public string UniqueCitizenNumber { get; set; }
+
+            [Phone]
+            [Display(Name = "Phone Number")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            public string UserName { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -70,19 +86,32 @@ namespace RentACar.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User
+                {
+                    UserName = Input.UserName,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    UniqueCitizenNumber = Input.UniqueCitizenNumber,
+                    PhoneNumber = Input.PhoneNumber
+                };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
                         values: new { userId = user.Id, code = code },
                         protocol: Request.Scheme);
+
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
@@ -96,6 +125,7 @@ namespace RentACar.Areas.Identity.Pages.Account
                         return LocalRedirect(returnUrl);
                     }
                 }
+
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
---
+++ RentACar/RentACar/Data/Models/UserServiceModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RentACar.Data.Entities;
+using RentACar.Data.Mapping;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class UserServiceModel : IdentityUser, IMapWith<User>
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string UniqueCitizenNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Request> Requests { get; set; }
+    }
+}
