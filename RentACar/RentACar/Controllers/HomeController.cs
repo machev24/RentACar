b8ebd144d24a46f48119547ce47954525041ead5
steliyan17 <stelianatanasov777@gmail.com>
2024-04-09 18:56:42 +0300

    груба обработка на home page

--- RentACar/RentACar/Controllers/HomeController.cs
+++ RentACar/RentACar/Controllers/HomeController.cs
@@ -1,36 +1,36 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using RentACar.Data.Models;
-using System.Diagnostics;
+using System.Collections.Generic;
 
-namespace RentACar.Controllers
+namespace CarRentalApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleManager)
+        // GET: Home
+        public ActionResult Index()
         {
-            _logger = logger;
-            _roleManager = roleManager;
-        }
+            // Списък с марки и модели коли (за демонстрационни цели)
+            var cars = new List<CarModel>
+            {
+                new CarModel { Brand = "Toyota", Model = "Corolla", Description = "Compact car" },
+                new CarModel { Brand = "Honda", Model = "Civic", Description = "Economy car" },
+                 new CarModel { Brand = "BMW", Model = "E46", Description = "Sport car" },
+                  new CarModel { Brand = "Opel", Model = "Zafira", Description = "Family car" },
+                   new CarModel { Brand = "Volkswagen", Model = "Golf", Description = "Economy car" },
+                    new CarModel { Brand = "Mercedes", Model = "CLS", Description = "Sport car" },
+                 new CarModel { Brand = "Porche", Model = "Panamera", Description = "Sport car" },
+                  new CarModel { Brand = "Citroen", Model = "C4", Description = "Economy car" },
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+            };
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(cars);
         }
     }
+
+    public class CarModel
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+    }
 }
+
